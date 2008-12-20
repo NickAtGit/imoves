107f5641c73558c51ad4085fde6adb2345b429d9
wilke.brian <wilke.brian@ca6120ac-cada-11dd-86d7-074df07e0730>
2008-12-20 09:02:19 +0000

    Added GPL license notice and copyright to source files.

--- Classes/iMovesAppDelegate.h
+++ Classes/iMovesAppDelegate.h
@@ -3,8 +3,13 @@
 //  iMoves
 //
 //  Created by Brian Wilke on 11/18/08.
-//  Copyright Fluke Networks 2008. All rights reserved.
 //
+
+// Copyright 2008 Brian Wilke.  All rights reserved.
+
+// This program is distributed under the terms of the
+// GNU General Public License.  A copy of this license
+// is availabe in the "GPL License" folder as gpl.txt.
 
 #import <UIKit/UIKit.h>
 
