c213feea359a75776feeef23227e2333b2a3beae
wilke.brian <wilke.brian@ca6120ac-cada-11dd-86d7-074df07e0730>
2008-12-20 09:02:19 +0000

    Added GPL license notice and copyright to source files.

--- trunk/Classes/GestureVisualizer.h
+++ trunk/Classes/GestureVisualizer.h
@@ -5,6 +5,12 @@
 //  Created by Brian Wilke on 12/7/08.
 //
 
+// Copyright 2008 Brian Wilke.  All rights reserved.
+
+// This program is distributed under the terms of the
+// GNU General Public License.  A copy of this license
+// is availabe in the "GPL License" folder as gpl.txt.
+
 #import <UIKit/UIKit.h>
 
 #import "Gestures.h"
